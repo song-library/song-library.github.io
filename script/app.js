dd5d1f11c4de9a11af30de4ce91e78918fa76352
NikolaKarov <nikola.a.karov@gmail.com>
2021-04-08 09:20:15 +0300

    More dependency changes

--- script/app.js
+++ script/app.js
@@ -1,5 +1,4 @@
 import page from "../node_modules/page/page.mjs";
-// import { render } from "../node_modules/lit-html/lit-html.js";
 import { render } from "https://unpkg.com/lit-html?module";
 
 import { homePage } from "./views/home.js";
--- script/views/navigation.js
+++ script/views/navigation.js
@@ -1,4 +1,3 @@
-// import { html, render } from "../../node_modules/lit-html/lit-html.js";
 import { html, render } from "https://unpkg.com/lit-html?module";
 import page from "../../node_modules/page/page.mjs";
 
